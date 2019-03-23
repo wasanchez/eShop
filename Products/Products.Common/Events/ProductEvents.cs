e2c179712edde807fb554b0efb505a369eccaa7d
wasanchez <williblues76@gmail.com>
2019-03-23 11:36:54 -0300

    ProductEventes

--- Products/Products.Common/Events/ProductEvents.cs
+++ Products/Products.Common/Events/ProductEvents.cs
@@ -6,6 +6,10 @@ namespace Products.Common.Events
 {
     public class ProducCreatedtEvent : Event
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
         [JsonConstructor]
         private ProducCreatedtEvent(Guid id, string name, string description, decimal price, int version) 
         : this (id, name, description, price) 
@@ -22,14 +26,59 @@ namespace Products.Common.Events
             Price = price;
         }
 
-        public ProducCreatedtEvent()
+    }
+
+    public class ProductDescriptionChanged : Event
+    {
+        public ProductDescriptionChanged(Guid id, string newDescription)
         {
+            Id = id;
+            NewDescription = newDescription;
         }
 
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
+        [JsonConstructor]
+        private ProductDescriptionChanged(Guid id, string newDescription, int version) : this (id, newDescription)
+        {
+            Version = version;
+        }
 
+        public string NewDescription { get; private set; }
 
+    }
+
+    public class ProductNameChanged : Event
+    {
+        public ProductNameChanged(Guid id, string newName)
+        {
+            Id = id;
+            NewName = newName;
+        }
+
+        [JsonConstructor]
+        private ProductNameChanged (Guid id, string newName, int version) : this (id, newName)
+        {
+            Version = version;
+        }
+
+        public string NewName { get; private set; }
+
+    }
+
+    public class ProductPriceChanged : Event
+    {
+        public decimal NewPrice  { get; private set; }
+
+        public ProductPriceChanged(Guid id, decimal price)
+        {
+            Id = id;
+            NewPrice = price;
+
+        }
+
+        [JsonConstructor]
+        private ProductPriceChanged (Guid id, decimal price, int version) : this(id, price)
+        {
+            Version = version;
+        }
     }
 }
--- Products/Products.Service/MicroServices/Products/Domain/Product.cs
+++ Products/Products.Service/MicroServices/Products/Domain/Product.cs
@@ -1,10 +1,29 @@
-﻿using MicroServices.Common.General;
+﻿using System;
+using MicroServices.Common.General;
 
 namespace Products.Service.MicroServices.Products.Domain
 {
     public class Product : Aggregate
     {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public double Price { get; set; }
 
-       
+
+        private Product () { }
+
+        public Product (Guid id, string name, string description, double price)
+        {
+            ValidateName(name);
+
+        }
+
+        private void ValidateName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Invalid name specified: cannot be empty.", new Exception("Invalid Product name"));
+            }
+        }
     }
 }
