f810ab7afcb027449212affbf7fc73f0d2912d28
wasanchez <williblues76@gmail.com>
2019-04-20 11:03:16 -0300

    Util classes

--- Common/MicroServices.Common.General/Util/ApiClient.cs
+++ Common/MicroServices.Common.General/Util/ApiClient.cs
@@ -34,11 +34,24 @@ namespace MicroServices.Common.General.Util
             return result;
         }
 
+        /// <summary>
+        /// Loads the many.
+        /// </summary>
+        /// <returns>The many.</returns>
+        /// <param name="uri">URI.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
         public static IEquatable<T> LoadMany<T>(string uri)
         {
             return Load<IEquatable<T>>(uri);
         }
 
+        /// <summary>
+        /// Creates a new instance of WebRequest.
+        /// </summary>
+        /// <returns>The request.</returns>
+        /// <param name="uri">URI.</param>
+        /// <param name="method">Method.</param>
+        /// <param name="contentType">Content type. By default 'text/jso'.</param>
         private static WebRequest CreateRequest(string uri, HttpMethod method, string contentType = "text/json")
         {
             var webRequest = WebRequest.Create(uri);
---
+++ Common/MicroServices.Common.General/Util/EventHandlerDiscovery.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MicroServices.Common.General.Util
+{
+    public class EventHandlerDiscovery
+    {
+        public Dictionary<Type,Aggregate> Handlers { get; private set; }
+
+        public EventHandlerDiscovery()
+        {
+            Handlers = new Dictionary<Type, Aggregate>();
+        }
+
+        public EventHandlerDiscovery Scan(Aggregate aggregate)
+        {
+            var handlerInterface = typeof(IHandler<>);
+            var aggType = aggregate.GetType();
+
+            var instances = from i in aggType.GetInterfaces()
+                            where (i.IsGenericType && handlerInterface.IsAssignableFrom(i.GetGenericTypeDefinition()))
+                            select i.GenericTypeArguments[0];
+
+            foreach (var i in instances)
+            {
+                Handlers.Add(i, aggregate);
+            }
+
+            return this;
+
+
+        }
+    }
+}
